8d9c4ec01fde3e6451c1dbbc859335e72f3cd0d4
Christoffer <zbcchhan11@zbc.dk>
2017-04-24 15:15:23 +0200

    Stylesheet added

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
 <html>
 <head>
+<link href="style.css" media="all" rel="stylesheet" />
 </head>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php require_once('header.php'); ?>
 
 <body>
-<?php require_once('index.html'); ?>
+<div class="content">
+Spread the TRUTH! Don't be a sheep, trump will cleanse the world!
+</div>
 </body>
 
 <?php require_once('footer.php'); ?>
