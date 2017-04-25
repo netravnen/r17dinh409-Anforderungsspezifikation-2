8c52e6dc551ef2c93f5eb5af7883974c1def1994
tobias gottfredsen <gogon003@gmail.com>
2017-04-25 15:31:06 +0200

    added content to contact us

--- about/contact-us.php
+++ about/contact-us.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cdh
- * Date: 25-04-2017
- * Time: 10:53
- */
+<?php require_once('../functions.php'); ?>
+<?php require_once('../header.php'); ?>
+
+<div class="container">
+<h1>Contact Address</h1><br>
+	<div class="row text-center">
+		<div class="col-sm-3 col-xs-6 first-box">
+        <h1><span class="glyphicon glyphicon-earphone"></span></h1>
+        <h3>Phone</h3>
+        <p>+880-1700-987654</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 second-box">
+        <h1><span class="glyphicon glyphicon-home"></span></h1>
+        <h3>Location</h3>
+        <p>725 5th Ave, New York, NY 10022, USA</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 third-box">
+        <h1><span class="glyphicon glyphicon-send"></span></h1>
+        <h3>E-mail</h3>
+        <p>info@thetruth.com</p><br>
+    </div>
+    <div class="col-sm-3 col-xs-6 fourth-box">
+    	<h1><span class="glyphicon glyphicon-leaf"></span></h1>
+        <h3>Web</h3>
+        <p>www.thetruth.com</p><br>
+    </div>
+	</div>
+</div>
+
+ <hr>
+        <?php footer(); ?>
+
+    </div> <!-- /container -->
+
+<?php require_once('../footer.php'); ?>
