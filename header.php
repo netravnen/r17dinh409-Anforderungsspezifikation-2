4d292facb5bdc9d46355dd001a60f22528b3524a
Christoffer <zbcchhan11@zbc.dk>
2017-04-25 11:04:56 +0200

    Move newsletter user view 1 level up

--- header.php
+++ header.php
@@ -1,5 +1,13 @@
 <html>
+
 <head>
-<link href="style.css" media="all" rel="stylesheet" />
+    <link href="style.css" media="all" rel="stylesheet"/>
 </head>
+
 <body>
+<div id="header">
+    <ol id="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="newsletter">Newsletter</a></li>
+    </ol>
+</div>
