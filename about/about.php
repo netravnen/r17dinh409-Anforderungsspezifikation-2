0787a3cacd82dc2deab82de57b4ccbe7746192dc
Christoffer <zbcchhan11@zbc.dk>
2017-04-25 16:21:07 +0200

    Updated about and contact code
    About: Removed call to file, because it is already in header.php
    ContactUs: Fixed an extra <div> closing tag

--- about/about.php
+++ about/about.php
@@ -1,4 +1,3 @@
-<?php require_once('../functions.php'); ?>
 <?php require_once('../header.php'); ?>
 
 
--- about/contact-us.php
+++ about/contact-us.php
@@ -1,34 +1,33 @@
 <?php require_once('../header.php'); ?>
 
 <div class="container">
-<h1>Contact Address</h1><br>
-	<div class="row text-center">
-		<div class="col-sm-3 col-xs-6 first-box">
-        <h1><span class="glyphicon glyphicon-earphone"></span></h1>
-        <h3>Phone</h3>
-        <p><?php echo constant('PHONE'); ?></p><br>
+    <h1>Contact Address</h1><br>
+    <div class="row text-center">
+        <div class="col-sm-3 col-xs-6 first-box">
+            <h1><span class="glyphicon glyphicon-earphone"></span></h1>
+            <h3>Phone</h3>
+            <p><?php echo constant('PHONE'); ?></p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 second-box">
+            <h1><span class="glyphicon glyphicon-home"></span></h1>
+            <h3>Location</h3>
+            <p><?php echo constant('LOCATION'); ?></p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 third-box">
+            <h1><span class="glyphicon glyphicon-send"></span></h1>
+            <h3>E-mail</h3>
+            <p><?php echo constant('EMAIL'); ?></p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 fourth-box">
+            <h1><span class="glyphicon glyphicon-leaf"></span></h1>
+            <h3>Web</h3>
+            <p><?php echo constant('WEB'); ?></p><br>
+        </div>
     </div>
-    <div class="col-sm-3 col-xs-6 second-box">
-        <h1><span class="glyphicon glyphicon-home"></span></h1>
-        <h3>Location</h3>
-        <p><?php echo constant('LOCATION'); ?></p><br>
-    </div>
-    <div class="col-sm-3 col-xs-6 third-box">
-        <h1><span class="glyphicon glyphicon-send"></span></h1>
-        <h3>E-mail</h3>
-        <p><?php echo constant('EMAIL'); ?></p><br>
-    </div>
-    <div class="col-sm-3 col-xs-6 fourth-box">
-    	<h1><span class="glyphicon glyphicon-leaf"></span></h1>
-        <h3>Web</h3>
-        <p><?php echo constant('WEB'); ?></p><br>
-    </div>
-	</div>
-</div>
 
- <hr>
-        <?php footer(); ?>
+    <hr>
+    <?php footer(); ?>
 
-    </div> <!-- /container -->
+</div> <!-- /container -->
 
 <?php require_once('../footer.php'); ?>
