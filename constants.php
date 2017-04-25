5d6613344d3732134890ad6f2fc5d0753ee92648
Christoffer <netravnen@users.noreply.github.com>
2017-04-25 15:47:03 +0200

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * CONTACT DETAILS
+ */
+DEFINE('PHONE','+880-1700-987654');
+DEFINE('LOCATION','725 5th Ave, New York, NY 10022, USA');
+DEFINE('EMAIL','info@thetruth.com');
+DEFINE('WEB','www.thetruth.com');
+?>
