6fd0ad0281f3bfcdca93413e7bc419b45f8235a0
tobias gottfredsen <gogon003@gmail.com>
2017-04-25 14:06:06 +0200

    added content to about

--- about/about.php
+++ about/about.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cdh
- * Date: 25-04-2017
- * Time: 10:44
- */
+<?php require_once('header.php'); ?>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container">
+            <h1>We are The Truth</h1>
+            <p>Our goals is to stop the visious lies that are spread around by all the devilish hillary supporters. There is no FAKE NEWS, They are all FACS. 
+            We aim to bring knowlegde to the youth, and wisdom into the minds of the Killary fan! WAKE UP SHEEP!
+            Just read our perfectly resonable and true articals and follow our campaigns, you will soon too no longer be a member of the sheeple!</p>
+                    </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Our team</h2>
+                <p>We are a team of higly educated amature jearnalists.
+                Tobias who was found on the streets of LA begging for food after he was atacked by a vicious pack of killary supporters at a anti-trump meeting.
+                Christoffer who was "allegedly" found guilty in asault after he atended a hillary meetup, we freed him from theese false acusations, and brought him to our HQ.                </p>
+                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Our goal</h2>
+                <p>We want to tell the world about our glorious leader and führer Donald J. Trump. Who was brought to this world by the lord himself, 
+                he rose down from the heavens to save us all from theese dire times, and is going to cleanse the world. </p>
+                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Why support The Truth?</h2>
+                <p>Simple.. it's The Truth. You can't argue with the facts, Trump is a great bussiness man and he knows how to Amarica great again.
+                He can stop the mexican invasions and save the amarican job market. He will shun all non-belivers, and clean the streets from all the filth. </p>
+                <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        </div>
+
+        <hr>
+
+        <footer>
+            <p>&copy; 2005-2017 The Truth INC. All Rights Reserved.</p>
+        </footer>
+    </div> <!-- /container -->
+
+<?php require_once('footer.php'); ?>
