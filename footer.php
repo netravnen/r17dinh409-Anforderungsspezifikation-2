c285d3bdbc5d1410a510edd3cbcc75fb917b6ece
tobias gottfredsen <gogon003@gmail.com>
2017-04-25 12:41:49 +0200

    added some more menus

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 
 <footer>
 </footer>
-
+<p> © 2005-2017 The Truth INC. All Rights Reserved. </p>
 </html>
--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <div id="header">
     <ul id="menu">
         <li><a href="/">Home</a></li>
+        <li><a href="About">About</a></li>
         <li><a href="newsletter">Newsletter</a></li>
         <li><a href="campaign">Campaign</a></li>
     </ul>
