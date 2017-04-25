b78f3c8ef81a4f096f709fffe091d0321b44cec4
Christoffer <netravnen@users.noreply.github.com>
2017-04-25 16:05:50 +0200

    Update header.php

--- header.php
+++ header.php
@@ -45,11 +45,11 @@ require_once('constants.php');
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Truth INC.</a>
+            <a class="navbar-brand" href="<?php echo constant('HOME'); ?>">Truth INC.</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="<?php echo constant('HOME'); ?>">Home</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                        aria-expanded="false">About<span class="caret"></span></a>
