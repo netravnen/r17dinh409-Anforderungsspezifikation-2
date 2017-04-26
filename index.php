b97c227d85b09a32421ae3a4056923809f9171c2
tobias gottfredsen <gogon003@gmail.com>
2017-04-26 14:13:42 +0200

    removed second title

--- index.php
+++ index.php
@@ -49,8 +49,7 @@
 
         <div class="row">
             <div class="col-lg-12">
-                <h1>Full Slider by Start Bootstrap</h1>
-                <p>The background images for the slider are set directly in the HTML using inline CSS. The rest of the styles for this template are contained within the <code>full-slider.css</code>file.</p>
+                <h1>Welcome to the truth!</h1>
             </div>
         </div>
 
