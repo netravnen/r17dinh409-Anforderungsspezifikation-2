2ec147ae4f0fcb8be5365f1e69303777823178fa
Christoffer <zbcchhan11@zbc.dk>
2017-04-26 10:15:10 +0200

    Added dummy db config

---
+++ db-config-dummy.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cdh
+ * Date: 26-04-2017
+ * Time: 10:12
+ */
+
+/* Database username */
+DEFINE('DB_USER','truth');
+
+/* Database password */
+DEFINE('DB_PASSWD','BqE1WJPWqO7N3wXv');
+
+/* Database table used */
+DEFINE('DB_TABLE','truth');
