6501e5ba25e5afff7af75c2531e79fd4f313dd54
tobias gottfredsen <gogon003@gmail.com>
2017-04-26 12:42:05 +0200

    added some content and remved non used classes to campaigns

--- campaigns/index.php
+++ campaigns/index.php
@@ -36,8 +36,8 @@
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Page Heading
-                    <small>Secondary Text</small>
+                <h1 class="page-header">Our Campaigns 
+                    <small>See the truth with your own eyes</small>
                 </h1>
             </div>
         </div>
@@ -47,14 +47,13 @@
         <div class="row">
             <div class="col-md-7">
                 <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+                    <img class="img-responsive" src="http://politicalgambler.com/wp-content/uploads/2015/10/trump-700x300.jpg" alt="">
                 </a>
             </div>
             <div class="col-md-5">
-                <h3>Project One</h3>
-                <h4>Subheading</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <h3>Bring forth The Truth</h3>
+                <h4>Spreading the right knowledge today and for ever!</h4>
+                <p>Trump is the messiah, brought to earth by the lord himself. Trump shall cleanse the world and we are here to spread this comon knowledge.</p>
             </div>
         </div>
         <!-- /.row -->
@@ -133,36 +132,6 @@
         <!-- /.row -->
 
         <hr>
-
-        <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <ul class="pagination">
-                    <li>
-                        <a href="#">&laquo;</a>
-                    </li>
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-                    <li>
-                        <a href="#">4</a>
-                    </li>
-                    <li>
-                        <a href="#">5</a>
-                    </li>
-                    <li>
-                        <a href="#">&raquo;</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <!-- /.row -->
 
         <hr>
 
