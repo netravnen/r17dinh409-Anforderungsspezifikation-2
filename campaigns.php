f105dee11640d19db5844f52def5c31afd4d30de
Christoffer <zbcchhan11@zbc.dk>
2017-04-26 13:02:14 +0200

    Updated campaigns.php

--- campaigns.php
+++ campaigns.php
@@ -1,132 +1,120 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php require_once('../header.php'); ?>
+<?php require_once('./header.php'); ?>
 
+<!-- Page Content -->
+<div class="container">
 
-<body>
-
-   
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Our Campaigns 
-                    <small>See the truth with your own eyes</small>
-                </h1>
-            </div>
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Our Campaigns
+                <small>See the truth with your own eyes</small>
+            </h1>
         </div>
-        <!-- /.row -->
-
-        <!-- Project One -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="http://politicalgambler.com/wp-content/uploads/2015/10/trump-700x300.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Bring forth The Truth</h3>
-                <h4>Spreading the right knowledge today and for ever!</h4>
-                <p>Trump is the messiah, brought to earth by the lord himself. Trump shall cleanse the world and we are here to spread this comon knowledge.</p>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Project Two -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="http://therightscoop.com/wp-content/uploads/2016/10/trump-wikileaks-700x300.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>There is no fake news!</h3>
-                <h4>Only the spoken Truth</h4>
-                <p>Trump only says fake news.. This is the vicious lies that Killary supporters spread to harm the name of Our Führer Donald J. Trump.
-                Do not listen to them, it is only lies, why would our gracious leader LIE. All things Trump says is proven facts.</p>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Project Three -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Project Three</h3>
-                <h4>Subheading</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
-                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Project Four -->
-        <div class="row">
-
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Project Four</h3>
-                <h4>Subheading</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
-                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Project Five -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="http://placehold.it/700x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3>Project Five</h3>
-                <h4>Subheading</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo, minima, inventore voluptatum saepe quos nostrum provident ex quisquam hic odio repellendus atque porro distinctio quae id laboriosam facilis dolorum.</p>
-                <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <hr>
-
-       
     </div>
-    <!-- /.container -->
+    <!-- /.row -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <!-- Project One -->
+    <div class="row">
+        <div class="col-md-7">
+            <a href="#">
+                <img class="img-responsive"
+                     src="http://politicalgambler.com/wp-content/uploads/2015/10/trump-700x300.jpg" alt="">
+            </a>
+        </div>
+        <div class="col-md-5">
+            <h3>Bring forth The Truth</h3>
+            <h4>Spreading the right knowledge today and for ever!</h4>
+            <p>Trump is the messiah, brought to earth by the lord himself. Trump shall cleanse the world and we are here
+                to spread this comon knowledge.</p>
+        </div>
+    </div>
+    <!-- /.row -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <hr>
 
-</body>
+    <!-- Project Two -->
+    <div class="row">
+        <div class="col-md-7">
+            <a href="#">
+                <img class="img-responsive"
+                     src="http://therightscoop.com/wp-content/uploads/2016/10/trump-wikileaks-700x300.jpg" alt="">
+            </a>
+        </div>
+        <div class="col-md-5">
+            <h3>There is no fake news!</h3>
+            <h4>Only the spoken Truth</h4>
+            <p>Trump only says fake news.. This is the vicious lies that Killary supporters spread to harm the name of
+                Our Führer Donald J. Trump.
+                Do not listen to them, it is only lies, why would our gracious leader LIE. All things Trump says is
+                proven facts.</p>
+        </div>
+    </div>
+    <!-- /.row -->
 
-</html>
- <hr>
-        <?php footer(); ?>
+    <hr>
 
-    </div> <!-- /container -->
+    <!-- Project Three -->
+    <div class="row">
+        <div class="col-md-7">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+            </a>
+        </div>
+        <div class="col-md-5">
+            <h3>Project Three</h3>
+            <h4>Subheading</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut
+                unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti
+                alias possimus!</p>
+            <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+    </div>
+    <!-- /.row -->
 
-<?php require_once('../footer.php'); ?>
+    <hr>
+
+    <!-- Project Four -->
+    <div class="row">
+
+        <div class="col-md-7">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+            </a>
+        </div>
+        <div class="col-md-5">
+            <h3>Project Four</h3>
+            <h4>Subheading</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem
+                officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in
+                suscipit?</p>
+            <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Project Five -->
+    <div class="row">
+        <div class="col-md-7">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+            </a>
+        </div>
+        <div class="col-md-5">
+            <h3>Project Five</h3>
+            <h4>Subheading</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo, minima, inventore voluptatum
+                saepe quos nostrum provident ex quisquam hic odio repellendus atque porro distinctio quae id laboriosam
+                facilis dolorum.</p>
+            <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+    <?php footer(); ?>
+
+</div> <!-- /container -->
+
+<?php require_once('./footer.php'); ?>
