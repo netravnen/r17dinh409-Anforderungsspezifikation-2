a431592514e4e86a785d87f35fdcff5af9a0d3e8
tobias gottfredsen <gogon003@gmail.com>
2017-04-27 14:58:37 +0200

    changed directories of css files on html code

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
     <title>The Big Picture - Start Bootstrap Template</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/lib/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css/the-big-picture.css" rel="stylesheet">
+    <link href="/lib/bootstrap/3.3.7/css/the-big-picture.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
