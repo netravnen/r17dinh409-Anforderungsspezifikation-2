764979a848fcd3b93ca5162860556a49a9d8291b
Christoffer <zbcchhan11@zbc.dk>
2017-04-27 13:19:52 +0200

    xmlapi.php: Realign indentions to project coding standards

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -3,13 +3,13 @@
 $xml_output = null;
 
 /* Query for DB info */
-require_once ('../db-config-dummy.php');
+require_once('../db-config-dummy.php');
 
 /* DB connection info */
 $host = "localhost";
-$user = constant ('DB_USER');
-$pass = constant ('DB_PASSWD');
-$database = constant ('DB_NAME');
+$user = constant('DB_USER');
+$pass = constant('DB_PASSWD');
+$database = constant('DB_NAME');
 
 /* Connect to DB host */
 $linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host.");
@@ -23,8 +23,9 @@ $query = "SELECT first_name,last_name,email FROM newsletter_subscribers";
 /* Store the rows from the DB in $resultID */
 $resultID = mysql_query($query, $linkID) or die("Data not found.");
 
-function replace_illegal_chars($str) {
-    return str_replace(array("&", "<", ">", "\"), array("&#38;", "&#60;", "&#62;", "&#92;"), $str);
+function replace_illegal_chars($str)
+{
+    return str_replace(array("&", "<", ">", "\"), array(" &#38;", "&#60;", "&#62;", "&#92;"), $str);
 }
 
 /* Set content-type */
@@ -35,7 +36,7 @@ $xml_output .= '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 $xml_output .= '<entries>' . "\n";
 
 /* Go through the rows and append to string $xml_output */
-for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
+for ($x = 0; $x < mysql_num_rows($resultID); $x++) {
     $row = mysql_fetch_assoc($resultID);
     $xml_output .= "\t" . '<entry>' . "\n";
     $xml_output .= "\t\t" . '<email>' . replace_illegal_chars($row['email']) . '</email>' . "\n";
