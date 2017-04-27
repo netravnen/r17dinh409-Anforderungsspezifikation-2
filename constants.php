e7edafe600a9d36f297378ce669a8073cd0f9991
Christoffer <netravnen@users.noreply.github.com>
2017-04-27 11:34:06 +0200

    Update constants.php
    Added section external links

--- constants.php
+++ constants.php
@@ -13,3 +13,9 @@ DEFINE('PHONE','+880-1700-987654');
 DEFINE('LOCATION','725 5th Ave, New York, NY 10022, USA');
 DEFINE('EMAIL','info@thetruth.com');
 DEFINE('WEBSITE','www.thetruth.com');
+
+/*
+ * EXTERNAL LINKS
+ */
+DEFINE('ASANA_DASHBOARD','https://app.asana.com/0/327192954341793/327257142271245');
+DEFINE('GITHUB_REPO','Https://github.com/dzbc/r17dinh410-Anforderungsspezifikation-2');
