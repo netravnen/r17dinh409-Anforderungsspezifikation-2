14ad2b2f33e1ef2f762107c006f16b3c85648f26
Christoffer <zbcchhan11@zbc.dk>
2017-04-27 15:01:10 +0200

    Updated add-subscriber.php

--- add-subscriber.php
+++ add-subscriber.php
@@ -10,7 +10,7 @@
 <?php require_once('./header.php'); ?>
 
 <?php
-if (isset($_SERVER['REQUEST_METHOD'] == 'POST')) {
+if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['emailAddress'])) {
     add_subscriber($_POST['firstName'], $_POST['lastName'], $_POST['emailAddress']);
     return;
 }
@@ -21,21 +21,20 @@ if (isset($_SERVER['REQUEST_METHOD'] == 'POST')) {
 <div class="container">
     <div class="starter-template">
         <h1><?php echo $title; ?></h1>
-        <p class="lead">
-            <form name="form1" method="post" action="">
-        <p>
-            <label for="firstName">First Name:</label>
-            <input type="text" name="firstName" id="firstName">
-        </p>
-        <p>
-            <label for="lastName">Last Name:</label>
-            <input type="text" name="lastName" id="lastName">
-        </p>
-        <p>
-            <label for="emailAddress">Email Address:</label>
-            <input type="text" name="emailAddress" id="emailAddress">
-        </p>
-        <input type="submit" value="Submit">
+        <form name="form1" method="post" action="">
+            <p>
+                <label for="firstName">First Name:</label>
+                <input type="text" name="firstName" id="firstName">
+            </p>
+            <p>
+                <label for="lastName">Last Name:</label>
+                <input type="text" name="lastName" id="lastName">
+            </p>
+            <p>
+                <label for="emailAddress">Email Address:</label>
+                <input type="text" name="emailAddress" id="emailAddress">
+            </p>
+            <input type="submit" value="Submit">
         </form>
     </div>
 
