d06d6df6fcce1fd72aa400a79d7d549c34ee68b3
Christoffer <netravnen@users.noreply.github.com>
2017-04-27 12:18:39 +0200

    Update xmlapi.php
    Added comments

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -1,36 +1,51 @@
  <?php
+
+/* Query for DB info */
 require_once ('../db-config-dummy.php');
+
+/* Set content-type */
 header("Content-type: text/xml");
 
+/* DB connection info */
 $host = "localhost";
 $user = constant ('DB_USER');
 $pass = constant ('DB_PASSWD');
 $database = constant ('DB_NAME');
 
+/* Connect to DB host */
 $linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host.");
+
+/* Look for the actual database */
 mysql_select_db($database, $linkID) or die("Could not find database.");
 
+/* Query the actual db table for rows */
 $query = "SELECT * FROM newsletter_subscribers ORDER BY email DESC";
+
+/* Store the rows from the DB in $resultID */
 $resultID = mysql_query($query, $linkID) or die("Data not found.");
 
-$xml_output = "<?xml version=\"1.0\"?>\n";
-$xml_output .= "<entries>\n";
+/* Begin Actual XML Output */
+$xml_output = '<?xml version="1.0"?>' . "\n";
+$xml_output .= '<entries>' . "\n";
 
+/* Go through the rows and append to string $xml_output */
 for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
     $row = mysql_fetch_assoc($resultID);
-    $xml_output .= "\t<entry>\n";
-    $xml_output .= "\t\t<email>" . $row['email'] . "</email>\n";
+    $xml_output .= "\t" . '<entry>' . "\n";
+    $xml_output .= "\t\t" . '<email>' . $row['email'] . '</email>' . "\n";
+
         // Escaping illegal characters
         $row['text'] = str_replace("&", "&", $row['text']);
         $row['text'] = str_replace("<", "<", $row['text']);
         $row['text'] = str_replace(">", "&gt;", $row['text']);
         $row['text'] = str_replace("\"", "&quot;", $row['text']);
-    $xml_output .= "\t\t<text>" . $row['text'] . "</text>\n";
-    $xml_output .= "\t</entry>\n";
+ 
+    $xml_output .= "\t\t" . '<text>' . $row['text'] . '</text>' . "\n";
+    $xml_output .= "\t" . '</entry>' . "\n";
 }
 
-$xml_output .= "</entries>";
+/* END of XML output */
+$xml_output .= '</entries>';
 
-print_r $xml_output;
-
-?>
+/* Print the whole lot */
+echo $xml_output;
