5892a03f4919d399cbd6e66626f6295805df4000
Christoffer <zbcchhan11@zbc.dk>
2017-04-27 14:58:48 +0200

    Updated add-subscriber.php

--- add-subscriber.php
+++ add-subscriber.php
@@ -9,38 +9,40 @@
 
 <?php require_once('./header.php'); ?>
 
+<?php
+if (isset($_SERVER['REQUEST_METHOD'] == 'POST')) {
+    add_subscriber($_POST['firstName'], $_POST['lastName'], $_POST['emailAddress']);
+    return;
+}
+?>
+
 <?php $title = 'Subscribe to ' . get_company_info('name') . ' newsletter:'; ?>
 
-    <div class="container">
-        <div class="starter-template">
-            <h1><?php echo $title; ?></h1>
-            <p class="lead">
-                <form action="add_subscriber()" method="post">
-            <p>
-                <label for="firstName">First Name:</label>
-                <input type="text" name="first_name" id="firstName">
-            </p>
-            <p>
-                <label for="lastName">Last Name:</label>
-                <input type="text" name="last_name" id="lastName">
-            </p>
-            <p>
-                <label for="emailAddress">Email Address:</label>
-                <input type="text" name="email" id="emailAddress">
-            </p>
-            <input type="submit" value="Submit">
-            </form>
-        </div>
+<div class="container">
+    <div class="starter-template">
+        <h1><?php echo $title; ?></h1>
+        <p class="lead">
+            <form name="form1" method="post" action="">
+        <p>
+            <label for="firstName">First Name:</label>
+            <input type="text" name="firstName" id="firstName">
+        </p>
+        <p>
+            <label for="lastName">Last Name:</label>
+            <input type="text" name="lastName" id="lastName">
+        </p>
+        <p>
+            <label for="emailAddress">Email Address:</label>
+            <input type="text" name="emailAddress" id="emailAddress">
+        </p>
+        <input type="submit" value="Submit">
+        </form>
+    </div>
 
-        <hr>
+    <hr>
 
-        <?php footer(); ?>
+    <?php footer(); ?>
 
-    </div> <!-- /container -->
+</div> <!-- /container -->
 
 <?php require_once('./footer.php'); ?>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    add_subscriber($_POST['first_name'], $_POST['last_name'], $_POST['email']);
-}
