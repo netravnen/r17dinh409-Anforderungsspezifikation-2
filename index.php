7a1b9aa12c0cec0e009aef9c7a5a6bb2ce41bd62
tobias gottfredsen <gogon003@gmail.com>
2017-04-27 15:23:53 +0200

    deleted unnecesary code

--- index.php
+++ index.php
@@ -4,28 +4,9 @@
 <!-- Make sure the <html> tag is set to the .full CSS class. Change the background image in the full.css file. -->
 
 
-<body>
- <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>The Truth Your number one source of true political facts!</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="/lib/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="/lib/bootstrap/3.3.7/css/the-big-picture.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 
     <!-- Page Content -->
     <div class="container">
@@ -39,11 +20,7 @@
     </div>
     <!-- /.container -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+   
 
 
     </div> <!-- /container -->
