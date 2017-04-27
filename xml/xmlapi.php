d6b6a3948e73efdea3fb8f3e2fa15c50531f0cd7
Christoffer <zbcchhan11@zbc.dk>
2017-04-27 13:31:25 +0200

    xmlapi.php: Changed api to use meekrodb.php library

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -1,51 +1,63 @@
 <?php
 
-$xml_output = null;
-
-/* Query for DB info */
-require_once('../db-config-dummy.php');
-
-/* DB connection info */
-$host = "localhost";
-$user = constant('DB_USER');
-$pass = constant('DB_PASSWD');
-$database = constant('DB_NAME');
-
-/* Connect to DB host */
-$linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host.");
-
-/* Look for the actual database */
-mysql_select_db($database, $linkID) or die("Could not find database.");
-
-/* Query the actual db table for rows */
-$query = "SELECT first_name,last_name,email FROM newsletter_subscribers";
-
-/* Store the rows from the DB in $resultID */
-$resultID = mysql_query($query, $linkID) or die("Data not found.");
-
-function replace_illegal_chars($str)
-{
-    return str_replace(array("&", "<", ">", "\"), array(" &#38;", "&#60;", "&#62;", "&#92;"), $str);
-}
-
-/* Set content-type */
+/*
+ * Set content-type
+ */
 header("Content-type: text/xml");
 
-/* Begin Actual XML Output */
-$xml_output .= '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+/**
+ * Use function to replace chars deemed illegal in output
+ */
+function replace_illegal_chars($str)
+{
+    return htmlentities($str, $flags = ENT_COMPAT | ENT_XML);
+}
+
+
+/*
+ * Require meekrodb
+ */
+require_once('../lib/meekrodb/2.3/meekrodb.2.3.class.php');
+
+/*
+ * Query for DB info
+ * */
+require_once('../db-config-dummy.php');
+
+/*
+ * DB connection info
+ */
+DB::$user = constant('DB_USER');
+DB::$password = constant('DB_PASSWD');
+DB::$dbName = constant('DB_NAME');
+
+/*
+ * Query the actual db table for rows
+ */
+$results = DB::query("SELECT * FROM newsletter_subscribers");
+
+/*
+ * Begin Actual XML Output
+ */
+$xml_output = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 $xml_output .= '<entries>' . "\n";
 
-/* Go through the rows and append to string $xml_output */
-for ($x = 0; $x < mysql_num_rows($resultID); $x++) {
-    $row = mysql_fetch_assoc($resultID);
+/*
+ * Go through the rows and append to string $xml_output
+ */
+foreach ($results as $row) {
     $xml_output .= "\t" . '<entry>' . "\n";
     $xml_output .= "\t\t" . '<email>' . replace_illegal_chars($row['email']) . '</email>' . "\n";
     $xml_output .= "\t\t" . '<text>' . replace_illegal_chars($row['first_name']) . ' ' . replace_illegal_chars($row['last_name']) . '</text>' . "\n";
     $xml_output .= "\t" . '</entry>' . "\n";
 }
 
-/* END of XML output */
+/*
+ * END of XML output
+ */
 $xml_output .= '</entries>';
 
-/* Print the whole lot */
+/*
+ * Print the whole lot
+ */
 echo $xml_output;
