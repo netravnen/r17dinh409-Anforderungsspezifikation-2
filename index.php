19452a461469570a85c165152c85a6b6676be73e
tobias gottfredsen <gogon003@gmail.com>
2017-04-27 14:53:04 +0200

    changed code

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
    <<html class="full" lang="en">
 <!-- Make sure the <html> tag is set to the .full CSS class. Change the background image in the full.css file. -->
 
-<head>
 
-    <meta charset="utf-8">
+<body>
+ <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -25,10 +25,6 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-</head>
-
-<body>
 
 
     <!-- Page Content -->
