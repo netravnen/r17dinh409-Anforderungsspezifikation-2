8fe61ff10433f3a53eff564c8f228435feb96c87
tobias gottfredsen <gogon003@gmail.com>
2017-04-27 11:50:47 +0200

    added XML PHP script

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -1,1 +1,36 @@
+ <?php
+require_once ('../db-config-dummy.php');
+header("Content-type: text/xml");
 
+$host = "localhost";
+$user = constant ('DB_USER');
+$pass = constant ('DB_PASSWD');
+$database =  constant ('DM_NAME');
+
+$linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host.");
+mysql_select_db($database, $linkID) or die("Could not find database.");
+
+$query = "SELECT * FROM newsletter_subscribers ORDER BY email DESC";
+$resultID = mysql_query($query, $linkID) or die("Data not found.");
+
+$xml_output = "<?xml version=\"1.0\"?>\n";
+$xml_output .= "<entries>\n";
+
+for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
+    $row = mysql_fetch_assoc($resultID);
+    $xml_output .= "\t<entry>\n";
+    $xml_output .= "\t\t<email>" . $row['email'] . "</email>\n";
+        // Escaping illegal characters
+        $row['text'] = str_replace("&", "&", $row['text']);
+        $row['text'] = str_replace("<", "<", $row['text']);
+        $row['text'] = str_replace(">", "&gt;", $row['text']);
+        $row['text'] = str_replace("\"", "&quot;", $row['text']);
+    $xml_output .= "\t\t<text>" . $row['text'] . "</text>\n";
+    $xml_output .= "\t</entry>\n";
+}
+
+$xml_output .= "</entries>";
+
+echo $xml_output;
+
+?>
