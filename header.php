4ee4b6f614955b60c7c63a732f30956c921c6dc4
Christoffer <netravnen@users.noreply.github.com>
2017-04-28 15:51:36 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/include/functions.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/constants.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/include/functions.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/meekrodb/2.3/meekrodb.2.3.class.php';
 if (explode('/', $_SERVER['REQUEST_URI'])[1] == 'cp') {
     require_once $_SERVER['DOCUMENT_ROOT'] . '/include/functions.cp.php';
