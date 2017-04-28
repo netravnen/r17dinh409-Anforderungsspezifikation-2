e46df05e4a94585bf15aa38d46db3f077a9443e8
Christoffer <zbcchhan11@zbc.dk>
2017-04-28 14:34:44 +0200

    Updated cp_index.php Added code to view all current newsletter subscribers. Code copied over from the deleted subscribers.php file

--- cp/index.php
+++ cp/index.php
@@ -1,5 +1,18 @@
-<?php
+<?php require_once('../header.php'); ?>
 
-require_once('../header.php');
+<?php $title = 'Manage subscribers to ' . get_company_info('name') . ' newsletter:'; ?>
 
-require_once('../footer.php');
+<div class="container">
+    <div class="starter-template">
+        <h1><?php echo $title; ?></h1>
+        <p class="lead">
+            <?php echo get_subscriber_info(); ?>
+    </div>
+
+    <hr>
+
+    <?php footer(); ?>
+
+<,/div> <!-- /container -->
+
+<?php require_once('../footer.php'); ?>
