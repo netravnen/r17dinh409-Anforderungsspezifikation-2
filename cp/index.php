8d6ba93fb059c94296580070f63c1dc1db8523ab
Christoffer <zbcchhan11@zbc.dk>
2017-05-01 15:14:15 +0200

    cp

--- cp/index.php
+++ cp/index.php
@@ -2,18 +2,17 @@
 
 <?php $title = 'Manage subscribers to ' . get_company_info('name') . ' newsletter:'; ?>
 
-<!--
-<?php
-$dir = dirname(__FILE__);
-echo "<p>Full path to this dir: " . $dir . "</p>";
-echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
-?>
--->
 <div class="container">
     <div class="starter-template">
         <h1><?php echo $title; ?></h1>
         <p class="lead">
             <?php echo get_subscriber_info(); ?>
+        </p>
+        <?php
+        $dir = dirname(__FILE__);
+        echo "<p>Full path to this dir: " . $dir . "</p>";
+        echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+        ?>
     </div>
 
     <hr>
