93c1e71df7bfc01d5eff3f470f05ae1668179bb5
Christoffer <zbcchhan11@zbc.dk>
2017-05-01 13:01:22 +0200

    Add config.php

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/* CONTACT DETAILS */
+DEFINE('COMPANY_NAME','The Truth Inc.');
+DEFINE('PHONE','+880-1700-987654');
+DEFINE('LOCATION','725 5th Ave, New York, NY 10022, USA');
+DEFINE('EMAIL','info@thetruth.com');
+DEFINE('WEBSITE','www.thetruth.com');
+
+/* EXTERNAL LINKS */
+DEFINE('ASANA_DASHBOARD','https://app.asana.com/0/327192954341793/327257142271245');
+DEFINE('GITHUB_REPO','Https://github.com/dzbc/r17dinh410-Anforderungsspezifikation-2');
+
+/* Database username */
+DEFINE('DB_USER','truth');
+
+/* Database password */
+DEFINE('DB_PASSWD','BqE1WJPWqO7N3wXv');
+
+/* Database name used */
+DEFINE('DB_NAME','truth');
+
+/* Database table used */
+DEFINE('DB_TABLE','newsletter_subscribers');
