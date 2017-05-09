1b6dcd54770627173c477178430882ca547c76c7
Christoffer <netravnen@users.noreply.github.com>
2017-05-09 09:36:28 +0200

    Update xmlapi.php

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -8,9 +8,9 @@ header("Content-type: text/xml");
 /*
  * Require other files
  */
-require_once('/classes/meekrodb.2.3.class.php');
-require_once('/config/config.php');
-require_once('/includes/functions.api.php');
+require_once('../classes/meekrodb.2.3.class.php');
+require_once('../config/config.php');
+require_once('../includes/functions.api.php');
 
 /*
  * DB connection info
