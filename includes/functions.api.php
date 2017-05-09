d3cc728f1713964825c91adc276e3db4139d96ab
Christoffer <netravnen@users.noreply.github.com>
2017-05-09 09:37:57 +0200

    Update functions.api.php

--- includes/functions.api.php
+++ includes/functions.api.php
@@ -9,3 +9,13 @@ function replace_illegal_chars($str)
 {
     return htmlentities($str, $flags = ENT_COMPAT | ENT_XML);
 }
+
+function get_db_connection()
+{
+    DB::$user = constant('DB_USER');
+    DB::$password = constant('DB_PASSWD');
+    DB::$dbName = constant('DB_NAME');
+    DB::$host = constant('DB_HOST');
+    DB::$port = constant('DB_PORT');
+    DB::$connect_options = array(MYSQLI_OPT_CONNECT_TIMEOUT => 10);
+}
