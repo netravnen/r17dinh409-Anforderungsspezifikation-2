5dedee256c1c1d53adb1fe711195f6f937548498
Christoffer <netravnen@users.noreply.github.com>
2017-05-09 09:41:48 +0200

    Update xmlapi.php

--- xml/xmlapi.php
+++ xml/xmlapi.php
@@ -15,7 +15,12 @@ require_once('../includes/functions.api.php');
 /*
  * DB connection info
  */
-get_db_connection();
+DB::$user = constant('DB_USER');
+DB::$password = constant('DB_PASSWD');
+DB::$dbName = constant('DB_NAME');
+DB::$host = constant('DB_HOST');
+DB::$port = constant('DB_PORT');
+DB::$connect_options = array(MYSQLI_OPT_CONNECT_TIMEOUT => 10);
 
 /*
  * Query the actual db table for rows
